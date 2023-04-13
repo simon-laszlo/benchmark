c257e8bbe1b0c71f0d42eb94761028525383fa59
lsimon <laszlo.simon@swisshttp.ch>
2023-04-13 09:00:25 +0200

    upgrade project dependencies

--- java/openjdk17_spring/src/main/java/benchmark/openjdk17_spring/CustomerController.java
+++ java/openjdk17_spring/src/main/java/benchmark/openjdk17_spring/CustomerController.java
@@ -2,8 +2,6 @@ package benchmark.openjdk17_spring;
 
 import java.util.List;
 
-import javax.servlet.http.HttpServletResponse;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.MediaType;
@@ -13,6 +11,8 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import jakarta.servlet.http.HttpServletResponse;
 
 
 @RestController
