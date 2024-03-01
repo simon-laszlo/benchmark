6edc5a7e13eed51900b3933ed0b09e2c79290a04
lsimon <laszlo.simon@swisshttp.ch>
2024-03-01 23:37:46 +0100

    test java virtual thread

---
+++ java/openjdk17_spring/src/main/java/benchmark/openjdk17_spring/CustomerServlet.java
@@ -0,0 +1,40 @@
+package benchmark.openjdk17_spring;
+
+import java.io.IOException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@Component
+public class CustomerServlet extends HttpServlet {
+	
+	@Autowired
+    private CustomerRepository customerRepository;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		final var customers = customerRepository.findAll();
+		final var content = objectMapper.writerWithDefaultPrettyPrinter().writeValueAsString(customers);
+		resp.setHeader("Content-Length", String.valueOf(content.length()));
+		resp.setHeader("Connection", null);
+		resp.getWriter().write(content);
+	}
+
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		super.doPost(req, resp);
+	}
+
+}
---
+++ java/openjdk17_spring/src/main/java/benchmark/openjdk17_spring/Servlet.java
@@ -0,0 +1,20 @@
+package benchmark.openjdk17_spring;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class Servlet {
+	
+	@Autowired
+	private CustomerServlet servlet;
+
+    @Bean
+    public ServletRegistrationBean<CustomerServlet> genericCustomServlet() {
+        ServletRegistrationBean<CustomerServlet> bean = new ServletRegistrationBean<>(servlet, "/customerss/*");
+        bean.setLoadOnStartup(1);
+        return bean;
+    }
+}
